aaf5f721667b657ef940c8e66fd5007e09be94d9
lívia <livcristinna@gmail.com>
2021-09-27 13:40:11 -0300

    create to-do

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -3,9 +3,14 @@ const criarTarefa = (evento) => {
     evento.preventDefault();
     const input = document.querySelector('[data-form-input]');
     const value = input.value;
-    console.log(value);
 
-    input.value = " "; // para limpar a caixa de texto da entrada; 
+    
+
+    const tarefa = document.querySelector('[data-task]');
+    const conteudo = `<p class="content"> ${valor}</p>`;
+
+    tarefa.innerHTML = conteudo; 
+    input.value = " "; // para limpar a caixa de texto da entrada;
 };
 
-novaTarefa.addEventListener('click', criarTarefa(evento));
+novaTarefa.addEventListener('click', criarTarefa);
