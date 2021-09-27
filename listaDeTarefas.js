e961286e61a0dc6028ef20bd19c1c2c70b67961f
lívia <livcristinna@gmail.com>
2021-09-27 13:30:42 -0300

    add Javascript

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,7 +1,11 @@
 const novaTarefa = document.querySelector('[data-form-button]');
-
-novaTarefa.addEventListener('click', () => {
+const criarTarefa = (evento) => {
+    evento.preventDefault();
     const input = document.querySelector('[data-form-input]');
     const value = input.value;
     console.log(value);
-});
+
+    input.value = " "; // para limpar a caixa de texto da entrada; 
+};
+
+novaTarefa.addEventListener('click', criarTarefa(evento));
