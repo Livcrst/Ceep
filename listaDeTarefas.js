fcd54c94c51861577ec581a8b132aaf5f12990d2
lívia <livcristinna@gmail.com>
2021-09-27 14:32:43 -0300

    create to-do

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,16 +1,34 @@
 const novaTarefa = document.querySelector('[data-form-button]');
 const criarTarefa = (evento) => {
+
     evento.preventDefault();
     const input = document.querySelector('[data-form-input]');
     const value = input.value;
+   
+    const Lista = document.querySelector('[data-list]');
+    const  tarefa = document.createElement('li');
 
-    
 
-    const tarefa = document.querySelector('[data-task]');
-    const conteudo = `<p class="content"> ${valor}</p>`;
+    tarefa.classList.add('task');
 
-    tarefa.innerHTML = conteudo; 
+    const conteudo = `<p class="content"> ${value}</p>`;
+    tarefa.innerHTML = conteudo;
+
+    tarefa.appendChild(BotaoConcluir());
+    Lista.appendChild(tarefa);
     input.value = " "; // para limpar a caixa de texto da entrada;
 };
 
 novaTarefa.addEventListener('click', criarTarefa);
+
+const BotaoConcluir = () => {
+
+    const botaoConcluir = document.createElement('button');
+
+    botaoConcluir.addEventListener('click', () => {
+        
+        console.log("TESTE: fui clicado")
+    });
+
+    return botaoConcluir;
+};
